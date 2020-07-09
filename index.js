069f134e51ffa1938b2b7a308d59627b0a4d5eda
Tim Allen <tim@noblesamurai.com>
2020-07-09 15:12:28 +1000

    semistandard

--- index.js
+++ index.js
@@ -12,101 +12,101 @@ const hasYarn = require('has-yarn');
 const DEFAULT_TEST_SCRIPT = 'echo "Error: no test specified" && exit 1';
 
 const PLURAL_OPTIONS = [
-	'env',
-	'global',
-	'ignore'
+  'env',
+  'global',
+  'ignore'
 ];
 
 const CONFIG_FILES = [
-	'.eslintrc.js',
-	'.eslintrc.yaml',
-	'.eslintrc.yml',
-	'.eslintrc.json',
-	'.eslintrc',
-	'.jshintrc',
-	'.jscsrc',
-	'.jscs.json',
-	'.jscs.yaml'
+  '.eslintrc.js',
+  '.eslintrc.yaml',
+  '.eslintrc.yml',
+  '.eslintrc.json',
+  '.eslintrc',
+  '.jshintrc',
+  '.jscsrc',
+  '.jscs.json',
+  '.jscs.yaml'
 ];
 
 const buildTestScript = test => {
-	if (test && test !== DEFAULT_TEST_SCRIPT) {
-		// Don't add if it's already there
-		if (!/^xo( |$)/.test(test)) {
-			return `xo && ${test}`;
-		}
+  if (test && test !== DEFAULT_TEST_SCRIPT) {
+    // Don't add if it's already there
+    if (!/^xo( |$)/.test(test)) {
+      return `xo && ${test}`;
+    }
 
-		return test;
-	}
+    return test;
+  }
 
-	return 'xo';
+  return 'xo';
 };
 
 const warnConfigFile = packageCwd => {
-	const files = CONFIG_FILES.filter(file => pathExists.sync(path.join(packageCwd, file)));
+  const files = CONFIG_FILES.filter(file => pathExists.sync(path.join(packageCwd, file)));
 
-	if (files.length === 0) {
-		return;
-	}
+  if (files.length === 0) {
+    return;
+  }
 
-	console.log(`${files.join(' & ')} can probably be deleted now that you're using XO.`);
+  console.log(`${files.join(' & ')} can probably be deleted now that you're using XO.`);
 };
 
 module.exports = async (options = {}) => {
-	const packageResult = readPkgUp.sync({
-		cwd: options.cwd,
-		normalize: false
-	}) || {};
-	const packageJson = packageResult.package || {};
-	const packagePath = packageResult.path || path.resolve(options.cwd || '', 'package.json');
-	const packageCwd = path.dirname(packagePath);
+  const packageResult = readPkgUp.sync({
+    cwd: options.cwd,
+    normalize: false
+  }) || {};
+  const packageJson = packageResult.package || {};
+  const packagePath = packageResult.path || path.resolve(options.cwd || '', 'package.json');
+  const packageCwd = path.dirname(packagePath);
 
-	packageJson.scripts = packageJson.scripts || {};
-	packageJson.scripts.test = buildTestScript(packageJson.scripts.test);
+  packageJson.scripts = packageJson.scripts || {};
+  packageJson.scripts.test = buildTestScript(packageJson.scripts.test);
 
-	const cli = minimist(options.args || argv());
-	delete cli._;
+  const cli = minimist(options.args || argv());
+  delete cli._;
 
-	for (const option of PLURAL_OPTIONS) {
-		if (cli[option]) {
-			cli[`${option}s`] = arrify(cli[option]);
-			delete cli[option];
-		}
-	}
+  for (const option of PLURAL_OPTIONS) {
+    if (cli[option]) {
+      cli[`${option}s`] = arrify(cli[option]);
+      delete cli[option];
+    }
+  }
 
-	if (Object.keys(cli).length > 0) {
-		packageJson.xo = { env: 'mocha', extends: 'semistandard', ...packageJson.xo, ...cli };
-	}
+  if (Object.keys(cli).length > 0) {
+    packageJson.xo = { env: 'mocha', extends: 'semistandard', ...packageJson.xo, ...cli };
+  }
 
-	writePkg.sync(packagePath, packageJson);
+  writePkg.sync(packagePath, packageJson);
 
-	const post = () => {
-		warnConfigFile(packageCwd);
-	};
+  const post = () => {
+    warnConfigFile(packageCwd);
+  };
 
-	if (options.skipInstall) {
-		post();
-		return;
-	}
+  if (options.skipInstall) {
+    post();
+    return;
+  }
 
-	if (hasYarn(packageCwd)) {
-		try {
-			await execa('yarn', ['add', '--dev', '--ignore-workspace-root-check', 'xo'], {cwd: packageCwd});
-			post();
-		} catch (error) {
-			if (error.code === 'ENOENT') {
-				console.error('This project uses Yarn but you don\'t seem to have Yarn installed.\nRun `npm install --global yarn` to install it.');
-				return;
-			}
+  if (hasYarn(packageCwd)) {
+    try {
+      await execa('yarn', ['add', '--dev', '--ignore-workspace-root-check', 'xo'], { cwd: packageCwd });
+      post();
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.error('This project uses Yarn but you don\'t seem to have Yarn installed.\nRun `npm install --global yarn` to install it.');
+        return;
+      }
 
-			throw error;
-		}
+      throw error;
+    }
 
-		return;
-	}
+    return;
+  }
 
-	await execa('npm', ['uninstall', 'semistandard'], {cwd: packageCwd});
-	await execa('npm', ['install', '--save-dev', 'eslint-config-semistandard, eslint-config-standard, eslint-plugin-standard'], {cwd: packageCwd});
-	await execa('npm', ['install', '--save-dev', 'xo'], {cwd: packageCwd});
-	post();
+  await execa('npm', ['uninstall', 'semistandard'], { cwd: packageCwd });
+  await execa('npm', ['install', '--save-dev', 'eslint-config-semistandard, eslint-config-standard, eslint-plugin-standard'], { cwd: packageCwd });
+  await execa('npm', ['install', '--save-dev', 'xo'], { cwd: packageCwd });
+  post();
 };
